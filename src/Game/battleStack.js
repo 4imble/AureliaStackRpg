d8f445857d5c509866230ed343f35c57395abe03
4imble <gimble.net@gmail.com>
2015-07-01 15:26:53 +0100

    Monster is removed on "gameLoop" event.
    This is to simulate a player killing a monster on the battle stack.

--- src/Game/battleStack.js
+++ src/Game/battleStack.js
@@ -3,11 +3,17 @@ import Monster from "./monster";
 export default class BattleStack {
     constructor(eventAggregator) {
         this.eventAggregator = eventAggregator;
-        
+
         this.stack = [new Monster("Grumble")];
-        
+
         this.eventAggregator.subscribe('monsterAdded', monster => {
             this.stack.push(monster);
+        });
+
+        this.eventAggregator.subscribe('gameLoop', monster => {
+            if (this.stack.length) {
+                this.stack.shift();
+            }
         });
     }
 }
